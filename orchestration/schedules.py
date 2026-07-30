a70878d238359459a9a751bed284615aa487266d
Arkhemis <80684272+Arkhemis@users.noreply.github.com>
2026-07-30 23:33:00 +0200

    feat: add igdb_ingest_job and its schedule

--- orchestration/schedules.py
+++ orchestration/schedules.py
@@ -1,6 +1,6 @@
 from dagster import ScheduleDefinition
 
-from orchestration.jobs import daily_ingest_job
+from orchestration.jobs import daily_ingest_job, igdb_ingest_job
 
 daily_ingest_schedule = ScheduleDefinition(
     name="daily_ingest_schedule",
@@ -8,6 +8,14 @@ daily_ingest_schedule = ScheduleDefinition(
     cron_schedule="0 2 * * *",
 )
 
+
+igdb_schedule = ScheduleDefinition(
+    name="igdb_schedule",
+    job=igdb_ingest_job,
+    cron_schedule="0 3 * * *",
+)
+
 schedules = [
     daily_ingest_schedule,
+    igdb_schedule,
 ]
